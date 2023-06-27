db0c3966bdfc25f7e116af005a6dd8d11557f801
Emoaya <739146797@qq.com>
2023-06-27 18:27:31 +0800

    AlgorithmCode_2023/6/27

---
+++ src/test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+/**
+ * ClassName: test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Emoaya
+ * @Create 2023/6/27 18:26
+ * @Version 1.0
+ */
+public class test {
+    @Test
+    public void test(){
+
+    }
+}
